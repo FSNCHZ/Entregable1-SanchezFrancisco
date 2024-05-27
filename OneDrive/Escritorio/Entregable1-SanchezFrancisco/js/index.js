15b4e657c72e3384849274bdf05c4df9a6e4866c
FSNCHZ <snchzfrancisco28@gmail.com>
2024-05-27 18:52:39 -0300

    Entregable2-SanchezFrancisco

--- OneDrive/Escritorio/Entregable1-SanchezFrancisco/js/index.js
+++ OneDrive/Escritorio/Entregable1-SanchezFrancisco/js/index.js
@@ -1,50 +1,46 @@
-function solicitarNombre () {
-    let nombreTienda = prompt ("Ingrese el nombre de su tienda: ");
-    document.title = ("Stockeate - "+ nombreTienda);
-}
-solicitarNombre();
+let productos = JSON.parse(localStorage.getItem('productos')) || []
 
-const inventario = [];
-
-//Repetir function accion
-
-function repetirAccion (){
-    let continuar = true;
-    while (continuar){
-        acciones()
-        continuar = confirm('¿Desea continuar?')
+const agregarProducto = (nombre, cantidad, categoria) => {
+    const producto = {
+        id:crypto.randomUUID,
+        nombre,
+        cantidad,
+        categoria
     }
+    productos.push(producto);
+    localStorage.setItem('productos',JSON.stringify(productos));
+    return producto;
 }
 
-function accion () {
-    let accionSolicitada = prompt ("Escriba 'agregrar' para agregar un producto, o escriba 'inventario' para ver la lista de productos actual");
-    if (accionSolicitada == 'agregar'){
-        const item = {
-            nombre: nombreProducto,
-            cantidad: cantidadProducto,
-        }
-        nombreProducto = prompt ('Escriba el nombre del producto')
-        cantidadProducto = prompt ('Indique la cantidad del producto')
-        inventario.push (item)
-        alert ('¡El producto fue agregado!')
-        repetirAccion()
-    }
-    else if (accionSolicitada == 'inventario'){
-        if ((inventario.length == 0)){
-            alert('No hay productos en el inventario')
-            repetirAccion()
-        }
-        else{
-            for (const producto of inventario){
-                alert ('Nombre: ' + producto.nombre + " Cantidad: " + cantidadItem)
-                repetirAccion()
-            }
-        }
-    }
-    else {
-        alert('La acción no es valida')
-        repetirAccion()
-    }
+const borrarProducto = (nombre) => {
+    productos = productos.filter(producto => producto.nombre != nombre);
+    localStorage.setItem('productos',JSON.stringify(productos))
 }
 
+const nuevaCardProducto = (producto) => {
+    const main = document.querySelector('#main');
+    const element = document.createElement('div');
+    element.className = "card-producto"
+    element.innerHTML = `
+                    <p>Nombre del producto: ${producto.nombre}</p>
+                    <p>Cantidad del producto: ${producto.cantidad}</p>
+                    <p>Categoria del producto: ${producto.categoria}</p>                    
+    `
+    main.append(element)
+}
 
+const principal = () => {
+    productos.forEach(producto => {
+        nuevaCardProducto(producto)
+    });
+    const btnAgregar = document.querySelector('#btnAgregar');
+    btnAgregar.addEventListener('click',()=>{
+        const nombreNuevoProducto = document.querySelector('#nombreNuevoProducto');
+        const cantidadNuevoProducto = document.querySelector('#cantidadNuevoProducto');
+        const categoriaNuevoProducto = document.querySelector('#categoriaNuevoProducto');
+        agregarProducto(nombreNuevoProducto.value, cantidadNuevoProducto.value, categoriaNuevoProducto.value);
+    }
+    )
+}
+
+principal()
